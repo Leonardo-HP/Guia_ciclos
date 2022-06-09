01a6b238990a126e176fd14e66e81ccdb6d757f1
Leonardo HP <ba.leonardohp@gmail.com>
2022-06-08 22:29:06 -0500

    Arrow Functions

---
+++ funciones.js
@@ -0,0 +1,49 @@
+//ESTOS SON EJEMPLOS DE VARIABLES GLOBALES
+
+// function saludoDirecto(){
+// alert( "Hola " + nombreCompleto)
+// }
+
+// function cambiarNombre(){
+//     nombreCompleto = prompt("Ingresa un nuevo nombre:")
+
+// }
+
+let edad = 47
+
+// VARIABLES LOCALES
+
+// Una variable dentro de una funcion es una variable local y solo puede usarse dentro de la misma 
+
+// function saludoDirecto2(){
+//     alert( "Hola " + nombreConcatenado)
+//     }
+
+// function concatenarNombres(){
+//     let nombreConcatenado = "Coder"+"House"
+//     console.log(nombreConcatenado)
+// }
+
+
+// FUNCIONES ANONIMAS
+
+// const cerramosClase  = function () {
+//     consol.warn("Ya se termina la clase")
+// }
+
+// cerramosClase()
+
+// ARROW FUNCTION
+// Una particularidad de las arrow function es que se pueden simplificar
+// const yaEsLaHora = (param1,param2) => {
+// }
+
+// const devolverResultado = (param1) =>{
+//     return param1 * 1.25
+// }
+
+// const devolverResultado = param1 => return param1 * 1.25{
+
+// }
+
+// const devolverResultado = param1 => param1 * 1.25
---
+++ variables.js
@@ -0,0 +1,4 @@
+// ESTAS SON VARIABLES GLOBALES
+
+let nombreCompleto  = "Julian Conti"
+let nombre = "John Doe"
