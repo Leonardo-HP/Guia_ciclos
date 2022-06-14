b4a6595a41d0f0920a60493bf65afdca34f9ce2e
Leonardo HP <ba.leonardohp@gmail.com>
2022-06-14 02:02:56 -0500

    FOR OF ARRAY

--- arrayproductos.js
+++ arrayproductos.js
@@ -26,13 +26,38 @@ function agregarProducto(){
 }
 
 
-
-
-
-
-
-
-
+//este es un generador de numeros random que regrea un numero del 0 al 1 por lo que lo 
+//multiplicamos por 1000 para que nos genere una id para nuestros productos
+//en numeros enteros, por lo cual tambien agregamos el parseint
 function creoID() {
     return parseInt(Math.random()* 1000)
 }
+
+generadorAutomatico()
+function generadorAutomatico(){
+    productos.push(new Producto(1234,"notebook ECO E17",15000))
+    productos.push(new Producto(1235,"hamster electronico",15000))
+
+}
+
+// la combinacion de objetos + arrays genera estructuras complejas de datos 
+//los metodos de arrays y las herramientas para recorrerlos nos permiten acceder 
+//y manipular todos estos datos de forma prolija
+
+    // FOR OF
+// la sentencia for..of permite recorrer un array ejecutando un bloque de codigo por cada elemento del objeto 
+//podemos enlistar nuestros objetos dentro del array con este ciclo
+//de esta manera podemos imprimir los elementos que estan en nuestro array
+
+function iterrarArray(){
+    for(let producto of productos){
+        console.table(producto)
+    }
+}
+
+// es un iterador que recorre el array de principio a fin y en cada iteracion accedemos al elemento en cuestion a travez
+//de la referencia que declaramos 
+//por cada iteracion se ejecuta el bloque de codigo que definimos ente llaves
+
+
+
